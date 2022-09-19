50a65a25b3a58b2501d8d6761705bcb3e3f9325f
Juan Diego Moreno <jdiiego95@gmail.com>
2022-09-19 07:36:23 -0500

    add laboratorioWebAPI

---
+++ Code/laboratorioWebApi/WebApi/Controllers/PersonaController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("persona")]
+    public class PersonaController : ControllerBase 
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarPersona()
+        {
+            //Todo el codigo
+
+            List<Persona> personas = new List<Persona>
+            {
+                new Persona
+                {
+                    id = "1",
+                    correo = "google@gmail.com",
+                    edad = "19",
+                    nombre = "Bernardo Peña"
+                },
+                new Persona
+                {
+                    id = "2",
+                    correo = "miguelgoogle@gmail.com",
+                    edad = "23",
+                    nombre = "Miguel Mantilla"
+                }
+            };
+
+            return personas;
+        }
+    
+
+        [HttpGet]
+        [Route("listarxid")]
+        public dynamic listarPersonaxid(int codigo)
+        {
+            //obtienes Persona de la db
+
+            return new Persona
+            {
+                id = codigo.ToString(),
+                correo = "google@gmail.com",
+                edad = "19",
+                nombre = "Bernardo Peña"
+            };
+        }
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarPersona(Persona persona)
+        {
+            //Guardar en la db y le asignas un id
+            persona.id = "3";
+
+            return new
+            {
+                success = true,
+                message = "registrado",
+                result = persona
+            };
+        }
+        [HttpPost]
+        [Route("eliminar")]
+        public dynamic eliminarPersona(Persona persona)
+        {
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+            //eliminas en la db
+
+            if (token != "juan123.")
+            {
+                return new
+                {
+                    success = false,
+                    message = "token incorrecto",
+                    result = ""
+                };
+            }
+
+            return new
+            {
+                success = true,
+                message = "eliminado",
+                result = persona
+            };
+        }
+    }
+}
---
+++ Code/laboratorioWebApi/WebApi/Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class Persona
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+    }
+}
